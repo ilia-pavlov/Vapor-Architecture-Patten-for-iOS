8d9c5a3a5a7e2aee1c1b34521dd41b1582888622
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-09 22:18:33 -0400

    Initial Commit

---
+++ Vapor Architecture Patten for iOS/Vapor_Architecture_Patten_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vapor_Architecture_Patten_for_iOSApp.swift
+//  Vapor Architecture Patten for iOS
+//
+//  Created by Ilia Pavlov on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Vapor_Architecture_Patten_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
